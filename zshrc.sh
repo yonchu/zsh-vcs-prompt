23200a8ab2efda3b544bd33b22bab0c8b3e2dd0b
yonchu <yuyuchu3333@gmail.com>
2012-12-13 10:41:24 +0900

    Add bash formats variables

--- zshrc.sh
+++ zshrc.sh
@@ -51,75 +51,103 @@ ZSH_VCS_PROMPT_CLEAN_SIGIL=${ZSH_VCS_PROMPT_CLEAN_SIGIL:-'✔ '}
 #   #i : The stashed status.
 #   #j : The clean status.
 
-## Git.
-# No action.
-if [ -z "$ZSH_VCS_PROMPT_GIT_FORMATS" ]; then
-    # VCS name
-    ZSH_VCS_PROMPT_GIT_FORMATS='(%{%B%F{yellow}%}#s%{%f%b%})'
-    # Branch name
-    ZSH_VCS_PROMPT_GIT_FORMATS+='[%{%B%F{red}%}#b%{%f%b%}'
-    # Ahead and Behind
-    ZSH_VCS_PROMPT_GIT_FORMATS+='#c#d|'
-    # Staged
-    ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{blue}%}#e%{%f%b%}'
-    # Conflicts
-    ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{red}%}#f%{%f%b%}'
-    # Unstaged
-    ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{yellow}%}#g%{%f%b%}'
-    # Untracked
-    ZSH_VCS_PROMPT_GIT_FORMATS+='#h'
-    # Stashed
-    ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{cyan}%}#i%{%f%b%}'
-    # Clean
-    ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{green}%}#j%{%f%b%}]'
-fi
-# No action using python.
-if [ -z "$ZSH_VCS_PROMPT_GIT_FORMATS_USING_PYTHON" ]; then
+if [ -n "$BASH_VERSION" ]; then
+    ### Bash
+    ## Git.
+    # No action.
+    if [ -z "$ZSH_VCS_PROMPT_GIT_FORMATS" ]; then
+        ZSH_VCS_PROMPT_GIT_FORMATS=' (#s)[#b#c#d|#e#f#g#h#i#j]'
+    fi
+    # No action using python.
     # Default is empty.
-    ZSH_VCS_PROMPT_GIT_FORMATS_USING_PYTHON=''
-fi
-# Action.
-if [ -z "$ZSH_VCS_PROMPT_GIT_ACTION_FORMATS" ]; then
-    # VCS name
-    ZSH_VCS_PROMPT_GIT_ACTION_FORMATS='(%{%B%F{yellow}%}#s%{%f%b%})'
-    # Branch name
-    ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='[%{%B%F{red}%}#b%{%f%b%}'
-    # Action
-    ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+=':%{%B%F{red}%}#a%{%f%b%}'
-    # Ahead and Behind
-    ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='#c#d|'
-    # Staged
-    ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{blue}%}#e%{%f%}'
-    # Conflicts
-    ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{red}%}#f%{%f%}'
-    # Unstaged
-    ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{yellow}%}#g%{%f%}'
-    # Untracked
-    ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='#h'
-    # Stashed
-    ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{cyan}%}#i%{%f%}'
-    # Clean
-    ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{green}%}#j%{%f%}]'
-fi
+    if [ -z "$ZSH_VCS_PROMPT_GIT_FORMATS_USING_PYTHON" ]; then
+        ZSH_VCS_PROMPT_GIT_FORMATS_USING_PYTHON=''
+    fi
+    # Action.
+    if [ -z "$ZSH_VCS_PROMPT_GIT_ACTION_FORMATS" ]; then
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS=' (#s)[#b:#a#c#d|#e#f#g#h#i#j]'
+    fi
 
-## Other vcs.
-# No action.
-if [ -z "$ZSH_VCS_PROMPT_VCS_FORMATS" ]; then
-    # VCS name
-    ZSH_VCS_PROMPT_VCS_FORMATS='(%{%B%F{yellow}%}#s%{%f%b%})'
-    # Branch name
-    ZSH_VCS_PROMPT_VCS_FORMATS+='[%{%B%F{red}%}#b%{%f%b%}]'
-fi
-# Action.
-if [ -z "$ZSH_VCS_PROMPT_VCS_ACTION_FORMATS" ]; then
-    # VCS name
-    ZSH_VCS_PROMPT_VCS_ACTION_FORMATS='(%{%B%F{yellow}%}#s%{%f%b%})'
-    # Branch name
-    ZSH_VCS_PROMPT_VCS_ACTION_FORMATS+='[%{%B%F{red}%}#b%{%f%b%}'
-    # Action
-    ZSH_VCS_PROMPT_VCS_ACTION_FORMATS+=':%{%B%F{red}%}#a%{%f%b%}]'
-fi
+    ## Other vcs.
+    # No action.
+    if [ -z "$ZSH_VCS_PROMPT_VCS_FORMATS" ]; then
+        ZSH_VCS_PROMPT_VCS_FORMATS=' (#s)[#b]'
+    fi
+    # Action.
+    if [ -z "$ZSH_VCS_PROMPT_VCS_ACTION_FORMATS" ]; then
+        ZSH_VCS_PROMPT_VCS_ACTION_FORMATS=' (#s)[#b:#a]'
+    fi
+else
+    ### ZSH
+    ## Git.
+    # No action.
+    if [ -z "$ZSH_VCS_PROMPT_GIT_FORMATS" ]; then
+        # VCS name
+        ZSH_VCS_PROMPT_GIT_FORMATS='(%{%B%F{yellow}%}#s%{%f%b%})'
+        # Branch name
+        ZSH_VCS_PROMPT_GIT_FORMATS+='[%{%B%F{red}%}#b%{%f%b%}'
+        # Ahead and Behind
+        ZSH_VCS_PROMPT_GIT_FORMATS+='#c#d|'
+        # Staged
+        ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{blue}%}#e%{%f%b%}'
+        # Conflicts
+        ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{red}%}#f%{%f%b%}'
+        # Unstaged
+        ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{yellow}%}#g%{%f%b%}'
+        # Untracked
+        ZSH_VCS_PROMPT_GIT_FORMATS+='#h'
+        # Stashed
+        ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{cyan}%}#i%{%f%b%}'
+        # Clean
+        ZSH_VCS_PROMPT_GIT_FORMATS+='%{%F{green}%}#j%{%f%b%}]'
+    fi
+    # No action using python.
+    if [ -z "$ZSH_VCS_PROMPT_GIT_FORMATS_USING_PYTHON" ]; then
+        # Default is empty.
+        ZSH_VCS_PROMPT_GIT_FORMATS_USING_PYTHON=''
+    fi
+    # Action.
+    if [ -z "$ZSH_VCS_PROMPT_GIT_ACTION_FORMATS" ]; then
+        # VCS name
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS='(%{%B%F{yellow}%}#s%{%f%b%})'
+        # Branch name
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='[%{%B%F{red}%}#b%{%f%b%}'
+        # Action
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+=':%{%B%F{red}%}#a%{%f%b%}'
+        # Ahead and Behind
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='#c#d|'
+        # Staged
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{blue}%}#e%{%f%}'
+        # Conflicts
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{red}%}#f%{%f%}'
+        # Unstaged
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{yellow}%}#g%{%f%}'
+        # Untracked
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='#h'
+        # Stashed
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{cyan}%}#i%{%f%}'
+        # Clean
+        ZSH_VCS_PROMPT_GIT_ACTION_FORMATS+='%{%F{green}%}#j%{%f%}]'
+    fi
 
+    ## Other vcs.
+    # No action.
+    if [ -z "$ZSH_VCS_PROMPT_VCS_FORMATS" ]; then
+        # VCS name
+        ZSH_VCS_PROMPT_VCS_FORMATS='(%{%B%F{yellow}%}#s%{%f%b%})'
+        # Branch name
+        ZSH_VCS_PROMPT_VCS_FORMATS+='[%{%B%F{red}%}#b%{%f%b%}]'
+    fi
+    # Action.
+    if [ -z "$ZSH_VCS_PROMPT_VCS_ACTION_FORMATS" ]; then
+        # VCS name
+        ZSH_VCS_PROMPT_VCS_ACTION_FORMATS='(%{%B%F{yellow}%}#s%{%f%b%})'
+        # Branch name
+        ZSH_VCS_PROMPT_VCS_ACTION_FORMATS+='[%{%B%F{red}%}#b%{%f%b%}'
+        # Action
+        ZSH_VCS_PROMPT_VCS_ACTION_FORMATS+=':%{%B%F{red}%}#a%{%f%b%}]'
+    fi
+fi
 
 ## The exe directory.
 ZSH_VCS_PROMPT_DIR=$(cd $(dirname $0) && pwd)
